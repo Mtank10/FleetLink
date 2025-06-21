b52f7ff2b8f9c6a6d5dcd6d39bacf35e5b160963
shivam raj <rajshivam691@gmail.com>
2025-06-22 01:18:17 +0530

    add docker file

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 
 app.use(cors({
-  origin: 'https://fleetlink-wg1k.onrender.com', 
+  origin: 'https://fleetlink-1-766x.onrender.com', 
   credentials: true, 
 }));
 app.use(express.json());
